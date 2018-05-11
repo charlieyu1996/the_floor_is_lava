f8195669bf1560543badab125cf042fc037506bc
manishjha4 <m2jah@edu.uwaterloo.ca>
2018-05-10 21:09:24 -0400

    Implemented the logic for tracking the intersection points

--- app/src/main/java/com/example/charlieyu/jump/GameView.java
+++ app/src/main/java/com/example/charlieyu/jump/GameView.java
@@ -14,10 +14,13 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.ViewPropertyAnimator;
 
+import java.lang.reflect.Array;
 import java.util.Observer;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
+
+import static com.example.charlieyu.jump.Point.lineLineIntersection;
 
 /**
  * Created by charlieyu on 2018-05-06.
@@ -192,7 +195,26 @@ public class GameView extends SurfaceView implements Runnable, Observer {
                 canvas.drawLine(initialPointRightX, initialPointY, x2, y2, paint);
 
             }
-*/
+          */
+            Point C = new Point(initialPointLeftX, initialPointY);
+            Point D = new Point(initialPointRightX, initialPointY);
+
+            if(sa.getCounter()>=8) {
+                for (int i = 0; i < 9; i++) {
+                    Point A = new Point(sa.getxNum()[i], sa.getyNum()[i]);
+                    for (int j=0; j<9; j++) {
+                        Point B = new Point(sa.getxNum2()[j], sa.getyNum2()[j]);
+                        Point intersection = lineLineIntersection(A, C, B, D);
+                        Point[] points = new Point[50];
+                        points[i] = intersection;
+                        Log.d("Intersection Points: ", String.valueOf(points[i].x + "," + points[i].y));
+                    }
+                }
+                canvas.drawLine(0, 0, (float) 540.0000137369143,(float) 1265.8528292298472, paint);
+                canvas.drawLine(0, 0, (float) 372.9033581921977,(float) 1131.1592251880948, paint);
+                canvas.drawLine(0, 0, (float) 707.0966740832372,(float) 1131.1592190333165, paint);
+                canvas.drawLine(0, 0, (float) 540.0000188855953,(float) 1021.6537898965632, paint);
+            }
 
 
 
@@ -200,6 +222,7 @@ public class GameView extends SurfaceView implements Runnable, Observer {
                 canvas.drawLine(initialPointLeftX, initialPointY, sa.getxNum()[i], sa.getyNum()[i], paint);
                 canvas.drawLine(initialPointRightX, initialPointY, sa.getxNum2()[i], sa.getyNum2()[i], paint);
             }
+
 
 
             surfaceHolder.unlockCanvasAndPost(canvas);
